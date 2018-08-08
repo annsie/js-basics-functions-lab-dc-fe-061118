bad9ebc386862525f75caba020341d7b5d13b37c
Ann Siebecker <asiebecker@hotmail.com>
2018-08-08 23:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickuplocation) {
+  return Math.abs(pickuplocation - 42)
+}
+function distanceFromHqInFeet (pickuplocation) {
+  return Math.abs(pickuplocation - 42) * 264
+}
+function distanceTravelledInFeet (start,finish) {
+  return Math.abs(start-finish) * 264
+}
